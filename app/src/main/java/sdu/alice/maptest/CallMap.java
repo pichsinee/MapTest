729f2cb78f8c6181829ac4241a6a140213e7ba72
pichsinee <p.pichsinee@gmail.com>
2017-03-30 16:53:33 +0700

    Initial commit

---
+++ app/src/main/java/sdu/alice/maptest/CallMap.java
@@ -0,0 +1,34 @@
+package sdu.alice.maptest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CallMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_map);
+
+
+    }   //Main Class
+
+    public void runMyMethod(View view){
+
+        EditText latitude = (EditText) findViewById(R.id.mapLat);
+        EditText longtitude = (EditText) findViewById(R.id.mapLong);
+        String lat = latitude.getText().toString().trim();
+        String lng = longtitude.getText().toString().trim();
+        Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:"+ lat + ","+ lng + ")");
+        //String geoUri = "http://maps.google.com/maps?q=loc:" + lat + "," + lng + " (" + mTitle + ")";
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        mapIntent.setPackage("com.google.android.apps.maps");   //if use GenyMotion , not use
+        startActivity(mapIntent);
+    }
+
+}   //Main Method
